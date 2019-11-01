e8983ac573fab3c057d64714d9c73f576453b639
Des Preston <despreston@gmail.com>
2019-11-01 16:04:42 -0400

    Cleanup. Adding LowestScoringUser interface

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,36 +1,37 @@
 export interface SlackMessage {
-  ts: string;
-  text: string;
-  user: string;
-  type: 'message',
-  team: string;
-  client_msg_id: string;
-  edited?: {
-    user: string;
-    ts: string;
-  }
+  readonly ts:            string;
+  readonly text:          string;
+  readonly user:          string;
+  readonly type:          'message';
+  readonly team:          string;
+  readonly client_msg_id: string;
+  sentiment:              number;
+  readonly edited?: {
+    readonly user: string;
+    readonly ts:   string;
+  },
 }
 
 export interface User {
-  id: string;
-  team_id: string,
-  name: string,
-  deleted: boolean,
-  color: string,
-  real_name: string,
-  tz: string;
-  tz_label: string;
-  tz_offset: number;
-  profile: {};
-  is_admin: boolean;
-  is_owner: boolean;
-  is_primary_owner: boolean;
-  is_restricted: boolean;
-  is_ultra_restricted: boolean;
-  is_bot: boolean;
-  is_app_user: boolean;
-  updated: number;
-  has_2fa: boolean;
+  readonly id:                  string;
+  readonly team_id:             string;
+  readonly name:                string;
+  readonly deleted:             boolean;
+  readonly color:               string;
+  readonly real_name:           string;
+  readonly tz:                  string;
+  readonly tz_label:            string;
+  readonly tz_offset:           number;
+  readonly profile:             {};
+  readonly is_admin:            boolean;
+  readonly is_owner:            boolean;
+  readonly is_primary_owner:    boolean;
+  readonly is_restricted:       boolean;
+  readonly is_ultra_restricted: boolean;
+  readonly is_bot:              boolean;
+  readonly is_app_user:         boolean;
+  readonly updated:             number;
+  readonly has_2fa:             boolean;
 }
 
 export interface UserMap {
@@ -39,18 +40,23 @@ export interface UserMap {
 
 export interface UserMapChatMeta {
   [ userId: string ]: {
-    msgCount: number;
+    msgCount:  number;
     sentiment: number;
   }
 }
 
 export interface PipelineStepArguments {
-  readonly users: UserMap;
+  readonly users:    UserMap;
   readonly messages: SlackMessage[];
-  readonly meta: UserMapChatMeta;
-  slackMsg?: Array<any>;  
+  readonly meta:     UserMapChatMeta;
+  slackMsg?:         Array<any>;
 }
 
 export interface PipelineStep {
   ( PipelineStepArguments ): void
+}
+
+export interface LowestScoreUser {
+  userId:  string;
+  score: number;
 }
