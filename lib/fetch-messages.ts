84b6519f979e233fdd40ae640fbd898bbda8a474
Des Preston <despreston@gmail.com>
2020-01-20 20:11:40 -0500

    fix "latest" ts of the first query

--- lib/fetch-messages.ts
+++ lib/fetch-messages.ts
@@ -3,6 +3,9 @@ import slack from './slack';
 import { WebAPICallResult } from '@slack/web-api';
 import conf from '../config';
 import { createSlackTs, slackTsToTime } from './utils/slack-timestamps';
+import axios from 'axios';
+
+const oneWeek: number = Date.now() - 6.048e8;
 
 interface SlackChannelHistoryResult extends WebAPICallResult {
   messages: SlackMessage[];
@@ -10,14 +13,14 @@ interface SlackChannelHistoryResult extends WebAPICallResult {
 
 function isBlankorSlackBot( message: SlackMessage ): boolean {
   const slackbot: string = 'USLACKBOT';
-  return !message.text || message.user && message.user === slackbot;
+  return !message.text || !message.user || message.user === slackbot;
 }
 
 async function fetchMessages(
   latest?: string,
   oldest?: string
 ): Promise<Array<SlackMessage>> {
-  const response = await slack.channels.history({
+  const response = await slack.conversations.history({
     channel: conf.channel,
     count: 1000,
     latest,
@@ -27,13 +30,31 @@ async function fetchMessages(
   return response.messages;
 }
 
+function filterBotAndOld( messages: SlackMessage[] ) {
+  let filtered: SlackMessage[] = [];
+
+  for ( const message of messages ) {
+    if ( isBlankorSlackBot( message ) ) {
+      continue;
+    }
+
+    // older than 1 week
+    if ( slackTsToTime( message.ts ) < oneWeek ) {
+      break;
+    }
+
+    filtered.push( message );
+  }
+
+  return filtered;
+}
+
 export default async function fetchAllMessages(): Promise<Array<SlackMessage>> {
-  const messages: SlackMessage[] = [];
+  let messages: SlackMessage[] = [];
   let fromFetch: Array<SlackMessage>;
-  const oneWeek: number = Date.now() - 6.048e8;
   const oneWeekSlackTs: string = createSlackTs( oneWeek );
 
-  fromFetch = await fetchMessages( null, oneWeekSlackTs );
+  fromFetch = await fetchMessages();
 
   let oldestMsg: SlackMessage = fromFetch.slice( -1 ).pop();
   let oldestTime: number = slackTsToTime( oldestMsg.ts );
@@ -43,19 +64,7 @@ export default async function fetchAllMessages(): Promise<Array<SlackMessage>> {
   let count: number = 0;
 
   while ( oldestTime >= oneWeek && count != stopLimit ) {
-    for ( const message of fromFetch ) {
-      if ( isBlankorSlackBot( message ) ) {
-        continue;
-      }
-
-      // older than 1 week
-      if ( slackTsToTime( message.ts ) < oneWeek ) {
-        break;
-      }
-
-      messages.push( message );
-    }
-
+    messages = [ ...messages, ...filterBotAndOld( fromFetch ) ];
     fromFetch = await fetchMessages( oldestMsg.ts );
     oldestMsg = fromFetch.slice( -1 ).pop();
     oldestTime = slackTsToTime( oldestMsg.ts );
